99fed16348d1f61c5d504f211352a74c693a9b76
amirali <amirali.mnj@gmail.com>
2020-09-29 02:56:41 +0330

    HW1 Docs added.

--- vigenere-cipher/main.cpp
+++ vigenere-cipher/main.cpp
@@ -6,9 +6,9 @@
 using namespace std;
 
 int main() {
-    VigenereEncoder cipher("AKEYQ");
+    VigenereEncoder cipher("KEY");
 //    string text = "A slumber did my spirit seal;\nI had no human fears:\nShe seemed a thing that could not feel\nThe touch of earthly years.\nNo motion has she now, no force;";
-    string text = "A slumber did my spirit seal; I had no human fears";
+    string text = "Hi this is a text";
     string encrypted = cipher.encrypt(text);
     string decrypted = cipher.decrypt(encrypted);
     cout << text << endl;
